945732884bde742650e1c08f3c14030a349492c3
Yin Lu <yinlu86@gmail.com>
2017-12-21 14:40:58 -0500

    Delete fileExist.py

--- src/miRge/utils/fileExist.py
+++
@@ -1,8 +0,0 @@
-def fileExist(fileName, sampleList):
-	state = False
-	sampleListNew = [item.split('.')[0] for item in sampleList]
-	for item in sampleListNew:
-		if item+'.fa' in fileName or item+'_raw.fa' in fileName:
-			state = True
-			break
-	return state
