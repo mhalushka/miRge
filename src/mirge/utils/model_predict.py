66f72ff1a6080c17926d10506e3f89e25be73c49
Yin Lu <yinlu86@gmail.com>
2018-01-05 15:43:01 -0500

    Add files via upload

--- src/mirge/utils/model_predict.py
+++ src/mirge/utils/model_predict.py
@@ -1,6 +1,8 @@
 import os
 import sys
 import pandas as pd
+import matplotlib
+matplotlib.use('agg')
 from sklearn.preprocessing import OneHotEncoder
 import numpy as np
 from sklearn.model_selection import train_test_split
